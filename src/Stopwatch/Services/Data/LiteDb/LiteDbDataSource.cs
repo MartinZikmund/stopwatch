e0408ef7201c5d55dc5b539ba22919a0545ec6cc
Martin Zikmund <martin@zikmund.dev>
2025-02-10 20:07:46 +0100

    Adjust error handling

--- src/Stopwatch/Services/Data/LiteDb/LiteDbDataSource.cs
+++ src/Stopwatch/Services/Data/LiteDb/LiteDbDataSource.cs
@@ -5,11 +5,13 @@ namespace Stopwatch.Services.Data.LiteDb;
 
 internal class LiteDbDataSource : IDataSource
 {
+	private readonly ILogger<LiteDbDataSource> _logger;
 	private bool _isInitialized;
 	private LiteDatabase? _liteDatabase;
 
-	public LiteDbDataSource()
+	public LiteDbDataSource(ILogger<LiteDbDataSource> logger)
 	{
+		_logger = logger;
 	}
 
 	public IStopwatchRepository Stopwatches { get; private set; } = null!;
@@ -36,33 +38,41 @@ internal class LiteDbDataSource : IDataSource
 	{
 		if (db.UserVersion == 0)
 		{
-			if (db.GetCollectionNames().Contains("StopwatchModel"))
+			try
 			{
-				var legacyCollection = db.GetCollection("StopwatchModel");
-				var newCollection = db.GetCollection<StopwatchModel>("Stopwatches");
-				foreach (var stopwatch in legacyCollection.FindAll())
+				if (db.GetCollectionNames().Contains("StopwatchModel"))
 				{
-					var stopwatchModel = new StopwatchModel();
-					stopwatchModel.Id = Guid.NewGuid().ToString();
-					stopwatchModel.Name = stopwatch["Name"].AsString;
-					stopwatchModel.Icon = stopwatch["Icon"].AsString;
-					stopwatchModel.BackgroundColor = stopwatch["BackgroundColor"].AsString;
-					stopwatchModel.BackgroundImageUri = stopwatch["BackgroundImageUri"].AsString;
-					stopwatchModel.BackgroundImageOpacity = stopwatch["BackgroundImageOpacity"].AsDouble;
-					stopwatchModel.InitialStartTime = stopwatch.TryGetValue("InitialStartTime", out var initialStartTimeValue) ? initialStartTimeValue.AsDateTime : null;
-					stopwatchModel.LastStartTime = stopwatch.TryGetValue("LastStartTime", out var lastStartTimeValue) ? lastStartTimeValue.AsDateTime : null;
-					stopwatchModel.PausedElapsedTime = TimeSpan.FromTicks(stopwatch["PausedElapsedTime"]);
-					stopwatchModel.Theme = Enum.TryParse<ElementTheme>(stopwatch["Theme"].AsString, out var theme) ? theme : ElementTheme.Default;
-					stopwatchModel.Laps = stopwatch["Laps"].AsArray.Select(lap => new LapModel
+					var legacyCollection = db.GetCollection("StopwatchModel");
+					var newCollection = db.GetCollection<StopwatchModel>("Stopwatches");
+					foreach (var stopwatch in legacyCollection.FindAll())
 					{
-						Note = lap["Note"].AsString,
-						TotalTime = TimeSpan.FromTicks(lap["TotalTime"])
-					}).ToArray();
+						var stopwatchModel = new StopwatchModel();
+						stopwatchModel.Id = Guid.NewGuid().ToString();
+						stopwatchModel.Name = stopwatch["Name"].AsString;
+						stopwatchModel.Icon = stopwatch["Icon"].AsString;
+						stopwatchModel.BackgroundColor = stopwatch["BackgroundColor"].AsString;
+						stopwatchModel.BackgroundImageUri = stopwatch["BackgroundImageUri"].AsString;
+						stopwatchModel.BackgroundImageOpacity = stopwatch["BackgroundImageOpacity"].AsDouble;
+						stopwatchModel.InitialStartTime = stopwatch.TryGetValue("InitialStartTime", out var initialStartTimeValue) ? initialStartTimeValue.AsDateTime : null;
+						stopwatchModel.LastStartTime = stopwatch.TryGetValue("LastStartTime", out var lastStartTimeValue) ? lastStartTimeValue.AsDateTime : null;
+						stopwatchModel.PausedElapsedTime = TimeSpan.FromTicks(stopwatch["PausedElapsedTime"]);
+						stopwatchModel.Theme = Enum.TryParse<ElementTheme>(stopwatch["Theme"].AsString, out var theme) ? theme : ElementTheme.Default;
+						stopwatchModel.Laps = stopwatch["Laps"].AsArray.Select(lap => new LapModel
+						{
+							Note = lap["Note"].AsString,
+							TotalTime = TimeSpan.FromTicks(lap["TotalTime"])
+						}).ToArray();
 
-					newCollection.Insert(stopwatchModel);
+						newCollection.Insert(stopwatchModel);
+					}
+
+					db.DropCollection("StopwatchModel");
 				}
-
-				db.DropCollection("StopwatchModel");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Failed to migrate data from legacy collection.");
+				// As this was a very early version of the app, data loss is not a problem here.
 			}
 
 			db.UserVersion = 1;
