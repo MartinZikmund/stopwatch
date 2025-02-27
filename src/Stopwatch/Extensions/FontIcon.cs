eabf34bad8688c5eefeb13acb9dab630211c0a79
Martin Zikmund <martin@zikmund.dev>
2025-02-27 16:16:19 +0100

    Adjust fonts on non-Windows targets

---
+++ src/Stopwatch/Extensions/FontIcon.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.UI.Xaml.Markup;
+
+namespace Stopwatch.Extensions.Xaml;
+
+[MarkupExtensionReturnType(ReturnType = typeof(FontIcon))]
+public class FontIconExtension : MarkupExtension
+{
+	public string Glyph { get; set; } = "";
+
+	protected override object ProvideValue() => new FontIcon() { Glyph = Glyph };
+}
