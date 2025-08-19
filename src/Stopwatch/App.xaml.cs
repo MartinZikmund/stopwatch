cda87b21fb01631b6964f13170f03c1cd2f28d81
Martin Zikmund <martin@zikmund.dev>
2025-08-19 08:29:41 +0200

    chore: Debug iOS startup 1

--- src/Stopwatch/App.xaml.cs
+++ src/Stopwatch/App.xaml.cs
@@ -86,26 +86,28 @@ public partial class App : Application
 		Ioc.Default.ConfigureServices(Host.Services);
 		await (Host.Services.GetRequiredService<IDataSource>()).InitializeAsync();
 
-		// Do not repeat app initialization when the Window already has content,
-		// just ensure that the window is active
-		if (MainWindow.Content is not WindowShell windowShell)
-		{
-			// Create a Frame to act as the navigation context and navigate to the first page
-			windowShell = new WindowShell(Host.Services, MainWindow);
+		MainWindow.Content = new BlankPage1();
 
-			// Place the frame in the current Window
-			MainWindow.Content = windowShell;
-		}
+		//// Do not repeat app initialization when the Window already has content,
+		//// just ensure that the window is active
+		//if (MainWindow.Content is not WindowShell windowShell)
+		//{
+		//	// Create a Frame to act as the navigation context and navigate to the first page
+		//	windowShell = new WindowShell(Host.Services, MainWindow);
 
-		var mainView = new MainView();
+		//	// Place the frame in the current Window
+		//	MainWindow.Content = windowShell;
+		//}
 
-		if (windowShell.RootFrame.Content is null)
-		{
-			// When the navigation stack isn't restored navigate to the first page,
-			// configuring the new page by passing required information as a navigation
-			// parameter
-			windowShell.ServiceProvider.GetRequiredService<INavigationService>().Navigate<MainViewModel>(args.Arguments);
-		}
+		//var mainView = new MainView();
+
+		//if (windowShell.RootFrame.Content is null)
+		//{
+		//	// When the navigation stack isn't restored navigate to the first page,
+		//	// configuring the new page by passing required information as a navigation
+		//	// parameter
+		//	windowShell.ServiceProvider.GetRequiredService<INavigationService>().Navigate<MainViewModel>(args.Arguments);
+		//}
 
 		// Ensure the current window is active
 		MainWindow.Activate();
